7dcb72d5769d027d80fb50a85140d89f2efc1aba
Lucas Popp <lucaspopp0@gmail.com>
2026-05-25 18:26:21 -0700

    delete locals.tf

--- modules/lambda-code-bucket/locals.tf
+++
@@ -1,7 +0,0 @@
-data "aws_caller_identity" "current" {}
-
-data "aws_region" "current" {}
-
-locals {
-  bucket_name = "${var.bucket_name_prefix}-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.region}"
-}
