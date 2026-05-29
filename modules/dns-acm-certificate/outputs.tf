d5202d0b22b1e321cded4d28e584cd63e762b765
Lucas Popp <lucaspopp0@gmail.com>
2026-05-29 16:53:01 -0700

    feat!: Improve cert validation options (#14)
    Adjusting cert validation options
    
    ---------
    
    Co-authored-by: rx-refill-reminders-writer[bot] <286820631+rx-refill-reminders-writer[bot]@users.noreply.github.com>

--- modules/dns-acm-certificate/outputs.tf
+++ modules/dns-acm-certificate/outputs.tf
@@ -1,4 +1,4 @@
 output "certificate_arn" {
   # Return validated certificate ARN if validation is enabled, otherwise the certificate ARN
-  value = var.validate && length(aws_acm_certificate_validation.validation) > 0 ? aws_acm_certificate_validation.validation[0].certificate_arn : aws_acm_certificate.certificate.arn
+  value = length(aws_acm_certificate_validation.validation) > 0 ? aws_acm_certificate_validation.validation[0].certificate_arn : aws_acm_certificate.certificate.arn
 }
