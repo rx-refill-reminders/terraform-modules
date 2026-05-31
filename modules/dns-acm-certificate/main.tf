3a22dffab71b1b2523758623fed67419a4778522
Lucas Popp <lucaspopp0@gmail.com>
2026-05-31 13:38:38 -0700

    dns validation method

--- modules/dns-acm-certificate/main.tf
+++ modules/dns-acm-certificate/main.tf
@@ -1,7 +1,7 @@
 resource "aws_acm_certificate" "certificate" {
   domain_name = var.domain_name
 
-  validation_method = var.validation.enabled ? "DNS" : null
+  validation_method = "DNS"
 
   lifecycle {
     create_before_destroy = true
